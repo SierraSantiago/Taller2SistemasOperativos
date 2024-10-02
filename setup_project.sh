720c5bf48622f18dbacf794b84d8c429a66a4995
SierraSantiago <santiagosierra003@gmail.com>
2024-10-02 08:38:14 -0500

    Punto 3

---
+++ setup_project.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo "Instalando dependencias desde requirements.txt..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+
+echo "Dependencias instaladas correctamente."
